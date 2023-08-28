8404ddd190aa0090f387359d2f341f24ba12100a
MelnykovSV <svmelnykov@gmail.com>
2023-08-29 00:09:41 +0300

    schemes layout done, css needs cleaning

--- src/components/ReactionSchemePreview/ReactionSchemePreview.styled.tsx
+++ src/components/ReactionSchemePreview/ReactionSchemePreview.styled.tsx
@@ -2,42 +2,13 @@ import styled from "@emotion/styled";
 
 const Container = styled.div`
   cursor: pointer;
-  width: 200px;
-  /* height: 250px; */
+  width: 300px;
   background-color: white;
-  /* overflow: hidden; */
   border: solid red 1px;
   padding: 10px 5px;
-  /* clip-path: inset(0 25%); */
   transition: width 0.3s linear, box-shadow 0.3s linear;
   font-size: 11px;
-
   position: relative;
-
-  &:hover {
-    z-index: 20 !important;
-    width: 300px;
-
-    box-shadow: rgba(0, 0, 0, 0.25) 0px 54px 55px,
-      rgba(0, 0, 0, 0.12) 0px -12px 30px, rgba(0, 0, 0, 0.12) 0px 4px 6px,
-      rgba(0, 0, 0, 0.17) 0px 12px 13px, rgba(0, 0, 0, 0.09) 0px -3px 5px;
-    .canvas-container {
-      width: 284px;
-      .arrows {
-        transform: translateX(0);
-      }
-      div:last-child {
-        transform: translateX(0);
-      }
-    }
-
-    .scheme-info {
-      width: 284px;
-      .scheme-info__additional {
-        transform: translateX(100%);
-      }
-    }
-  }
 
   .canvas-container {
     border: solid green 1px;
@@ -45,22 +16,22 @@ const Container = styled.div`
     margin-bottom: 20px;
     display: flex;
     transition: width 0.3s linear;
-    width: 140px;
+    width: 284px;
     div {
       background-color: white;
     }
     .arrows {
-      transition: transform 0.3s linear;
-      transform: translateX(-50px);
+      /* transition: transform 0.3s linear; */
+      transform: translateX(0);
     }
     div:last-child {
-      transition: transform 0.3s linear;
-      transform: translateX(-140px);
+      /* transition: transform 0.3s linear; */
+      transform: translateX(0);
     }
   }
   .scheme-info {
     border: solid blue 1px;
-    width: 142px;
+    width: 284px;
     transition: width 0.3s linear;
     margin: 0 auto;
     display: flex;
@@ -90,15 +61,21 @@ const Container = styled.div`
     .scheme-info__additional {
       width: 140px;
       transition: transform 0.3s linear;
-      transform: translateX(0);
+      transform: translateX(100%);
       position: absolute;
       padding: 10px;
     }
   }
 
-
-
   @media screen and (min-width: 768px) {
+    cursor: pointer;
+    width: 200px;
+    background-color: white;
+    border: solid red 1px;
+    padding: 10px 5px;
+    transition: width 0.3s linear, box-shadow 0.3s linear;
+    font-size: 11px;
+    position: relative;
     &:nth-child(3n + 2) {
       left: 50%;
       transform: translateX(-50%);
@@ -107,6 +84,86 @@ const Container = styled.div`
     &:nth-child(3n + 3) {
       left: 100%;
       transform: translateX(-100%);
+    }
+    .canvas-container {
+      border: solid green 1px;
+      margin: 0 auto;
+      margin-bottom: 20px;
+      display: flex;
+      transition: width 0.3s linear;
+      width: 140px;
+      div {
+        background-color: white;
+      }
+      .arrows {
+        transition: transform 0.3s linear;
+        transform: translateX(-50px);
+      }
+      div:last-child {
+        transition: transform 0.3s linear;
+        transform: translateX(-140px);
+      }
+    }
+    .scheme-info {
+      border: solid blue 1px;
+      width: 142px;
+      transition: width 0.3s linear;
+      margin: 0 auto;
+      display: flex;
+      flex-shrink: 0;
+      position: relative;
+      dl {
+        background-color: white;
+        margin: 0;
+
+        div {
+          display: flex;
+          justify-content: space-between;
+
+          dt,
+          dd {
+            margin: 0;
+          }
+        }
+      }
+      .scheme-info__main {
+        width: 140px;
+        transform: translateX(0);
+        padding: 10px;
+
+        /* position: absolute; */
+      }
+      .scheme-info__additional {
+        width: 140px;
+        transition: transform 0.3s linear;
+        transform: translateX(0);
+        position: absolute;
+        padding: 10px;
+      }
+    }
+    &:hover {
+      z-index: 20 !important;
+      width: 300px;
+
+      box-shadow: rgba(0, 0, 0, 0.25) 0px 54px 55px,
+        rgba(0, 0, 0, 0.12) 0px -12px 30px, rgba(0, 0, 0, 0.12) 0px 4px 6px,
+        rgba(0, 0, 0, 0.17) 0px 12px 13px, rgba(0, 0, 0, 0.09) 0px -3px 5px;
+      .canvas-container {
+        width: 284px;
+        .arrows {
+          transform: translateX(0);
+        }
+        div:last-child {
+          transform: translateX(0);
+        }
+      }
+
+      .scheme-info {
+        width: 284px;
+        .scheme-info__additional {
+          transform: translateX(100%);
+        }
+      }
     }
   }
   @media screen and (min-width: 1280px) {
