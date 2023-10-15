ad4066db78ff1aff5071ccdaa94a401b7b008de6
MelnykovSV <svmelnykov@gmail.com>
2023-10-15 03:38:07 +0300

    auth fixed

--- src/redux/auth/authSlice.ts
+++ src/redux/auth/authSlice.ts
@@ -125,5 +125,5 @@ export const userReducer = authSlice.reducer;
 export const { updateTokens, forceLogOut } = authSlice.actions;
 
 export const getAccessToken = (state: IState) => state.auth.accessToken;
-export const getIsLoggedIn = (state: IState) => state.auth.accessToken;
+export const getIsLoggedIn = (state: IState) => state.auth.isLoggedIn;
 export const getIsRefreshing = (state: IState) => state.auth.isRefreshing;
